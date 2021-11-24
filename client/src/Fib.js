5f23fd8a45fab012f5f71d93f658379dc4e0a999
= <30977494+cmagk@users.noreply.github.com>
2021-11-25 01:07:19 +0200

    fixed bug with too many requests on client

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -33,7 +33,7 @@ const Fib = () => {
   useEffect(() => {
     fetchValues();
     fetchIndexes();
-  }, [index]);
+  }, []);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
